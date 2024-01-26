ae82cbc7ed982ee2691dac87810273e9c50d8842
unknown <mmohamedabdelm@gmail.com>
2024-01-26 14:09:09 +0200

    add project 2 apps frontend , backend using django& vue.js

---
+++ Frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<todoApp>
+  
+</todoApp>
+</template>
+
+<script>
+import todoApp from './components/todoApp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    todoApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
---
+++ Frontend/src/components/todoApp.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">ToDo App</h2>
+    <div class="d-flex mt-5">
+      <input type="text" placeholder="Enter Task" class="w-100 form-control" v-model="task">
+      <button class="ms-2 btn btn-warning" @click="submitTask">add</button>
+    </div>
+
+    <table class="table mt-5 table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col">Edit</th>
+          <th scope="col">Delete</th>
+          <th scope="col">Check Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks" :key="task.id">
+          <th scope="row">{{ task.task }}</th>
+          <td :class="getStatusClass(task.status)">{{ task.status }}</td>
+          <td>
+            <div>
+              <span class="fa fa-pen" @click="editTask(index)"></span>
+            </div>
+          </td>
+          <td>
+            <div>
+              <span class="fa fa-trash" @click="deleteTask(index)"></span>
+            </div>
+          </td>
+          <td>
+            <div>
+              <span class="fa fa-check me-2" @click="changeStatus(index, 'finished')"></span>
+              <span class="fa fa-play me-2" @click="changeStatus(index, 'inprogress')"></span>
+              <span class="fa fa-spinner me-2" @click="changeStatus(index, 'todo')"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'todoApp',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      task: '',
+      editedtask: null,
+      tasks: []
+    }
+  },
+
+
+  mounted(){
+    this.getTasks();
+  },
+
+
+  methods: {
+
+    getTasks() {
+    // Using Axios to make a GET request to the specified URL
+    axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/todo/api/",
+    })
+    // Handling the response from the API
+    .then(response => { 
+      console.log(response.data)
+      // Updating the 'tasks' data property with the response data
+      this.tasks = response.data['results'];
+    });
+  },
+
+    submitTask() {
+      if (this.task === 0) return;
+      if (this.editedtask === null) {
+        this.tasks.push({
+          name: this.task,
+          status: 'todo',
+        });
+      } else {
+        this.tasks[this.editedtask].name = this.task;
+        this.editedtask = null;
+      }
+
+      this.task = '';
+    },
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedtask = index;
+    },
+
+    changeStatus(index, newStatus) {
+      this.tasks[index].status = newStatus;
+    },
+
+    getStatusClass(status) {
+      // Define your CSS classes based on task status
+      return {
+        'text-danger': status === 'finished',
+        'text-warning': status === 'inprogress',
+        'text-success': status === 'todo',
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
